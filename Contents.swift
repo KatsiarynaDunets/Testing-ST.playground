eb680c4196b9c310579b208c7d5e8ef91691937c
KatsiarynaDunets <kate.dunets@gmail.com>
2023-08-16 23:21:48 +0200

    QA

--- Contents.swift
+++ Contents.swift
@@ -5,3 +5,6 @@ var greeting = "Hello, playground"
 print("task 1 is completed")
 print("task 2 is completed")
 print("task 2.1 is completed")
+
+ //Now it's time to start QA process
+print("Hi QA team, please, check the edits")
